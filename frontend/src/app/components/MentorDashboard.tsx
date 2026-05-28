c32218051fe31b3637d1dbf242b7052cbdc5d21e
Jakab Gergo <jakabgergo23@gmail.com>
2026-05-28 14:21:49 +0300

    navigation bug after login fixed

--- frontend/src/app/components/MentorDashboard.tsx
+++ frontend/src/app/components/MentorDashboard.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router';
+import { Link, useNavigate } from 'react-router-dom';
 import { motion, AnimatePresence } from 'motion/react';
 import { 
   Users, 
